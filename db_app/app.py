8f64ff12c2c8e5aa9169533cbdb51ff7cc4adb9a
DuDlick <dannik25@ya.ru>
2023-08-28 00:07:37 +0300

    app

--- db_app/app.py
+++ db_app/app.py
@@ -1,17 +1,63 @@
-from flask import Flask
+from config import host, user, password, db_name
+import pymysql
 
-app = Flask(__name__)
+try:
+    connection = pymysql.connect(
+        host=host,
+        port=3306,
+        user=user,
+        password=password,
+        database=db_name,
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+    print('Connection success')
+    print('#' * 20)
+
+    try:
+        with connection.cursor() as cursor:
+            show_tables = "SHOW TABLES LIKE 'users';"
+            cursor.execute(show_tables)
+            table_exists = cursor.fetchone()
+
+        if not table_exists:
+            with connection.cursor() as cursor:
+                create_table = ("CREATE TABLE `users`"
+                                "(id int AUTO_INCREMENT,"
+                                "name varchar(100),"
+                                "password varchar(255),"
+                                "phone_number varchar(25),"
+                                "email varchar(255),"
+                                "PRIMARY KEY (id));")
+                cursor.execute(create_table)
+                print("Table is created")
+        else:
+            print('Table already exists')
+            print('#' * 20)
 
 
-@app.route('/')
-def index():
-    return "Hello World"
+        def add_value():
+            names = input('Read name: ')
+            passwords = input('Read password: ')
+            phone = input('Read phone number: ')
+            emails = input('Read email address: ')
 
 
-@app.route('/user')
-def users():
-    return "FuSk you"
+        with connection.cursor() as cursor:
+            # names = input('Read name: ')
+            # passwords = (input('Read password: '))
+            # phone = (input('Read phone number: '))
+            # emails = input('Read email address: ')
 
+            insert_query = "INSERT INTO `users` (name, password, phone_number, email) VALUES (%s, %s, %s, %s);"
+            cursor.execute(insert_query, (names, passwords, phone, emails))
+            connection.commit()
+            print('Entered values saved')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    finally:
+        cursor.close()
+        connection.close()
+
+except Exception as ex:
+    print("connection unavailable")
+    print(ex)
